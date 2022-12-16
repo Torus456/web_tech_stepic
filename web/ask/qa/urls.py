bd42b17568fecf9af1df466b7238bebd4c0071c6
Tours456 <odilich@yandex.ru>
2022-12-16 10:07:17 +0500

    fix main page

--- web/ask/qa/urls.py
+++ web/ask/qa/urls.py
@@ -4,10 +4,11 @@ from .import views
 app_name = "qa"
 # app_name will help us do a reverse look-up latter.
 urlpatterns = [
+    url(r'^$', views.test),
     url('login/', views.test),
     url('signup/', views.test),
     url(r'question/(?P<group_id>[^/]+)', views.test),
     url('ask/',  views.test),
     url('popular/', views.test),
-    url('new/', views.test)
+    url('new/', views.test),    
 ]
